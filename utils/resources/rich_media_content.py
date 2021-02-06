e655645824d153a6a9eb12420f38efaae1c230ff
Nikolai <kirichk@gmail.com>
2021-02-06 20:44:55 +0200

    fix

--- utils/resources/rich_media_content.py
+++ utils/resources/rich_media_content.py
@@ -2,13 +2,13 @@
 from ..tools import rich_message_consctructor
 
 
-RICH_MEDIA_SETS = rich_message_consctructor([])
-RICH_MEDIA_ROLLS = rich_message_consctructor([])
-RICH_MEDIA_PIZZA = rich_message_consctructor([])
-RICH_MEDIA_GUNCANS = rich_message_consctructor([])
-RICH_MEDIA_SUSHI = rich_message_consctructor([])
+RICH_MEDIA_SETS = rich_message_consctructor('')
+RICH_MEDIA_ROLLS = rich_message_consctructor('')
+RICH_MEDIA_PIZZA = rich_message_consctructor('')
+RICH_MEDIA_GUNCANS = rich_message_consctructor('')
+RICH_MEDIA_SUSHI = rich_message_consctructor('')
 RICH_MEDIA_COMBO = rich_message_consctructor('resources/images/combo')
-RICH_MEDIA_NUGGETS_WINGS = rich_message_consctructor([])
-RICH_MEDIA_MUSSILS = rich_message_consctructor([])
-RICH_MEDIA_SAUCES = rich_message_consctructor([])
-RICH_MEDIA_DRINKS = rich_message_consctructor([])
+RICH_MEDIA_NUGGETS_WINGS = rich_message_consctructor('')
+RICH_MEDIA_MUSSILS = rich_message_consctructor('')
+RICH_MEDIA_SAUCES = rich_message_consctructor('')
+RICH_MEDIA_DRINKS = rich_message_consctructor('')
