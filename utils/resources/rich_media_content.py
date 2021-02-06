d83ca16263e05a81bcb6cf35ba819eeb024c22c8
Nikolai <kirichk@gmail.com>
2021-02-06 23:55:12 +0200

    added whole media map

--- utils/resources/rich_media_content.py
+++ utils/resources/rich_media_content.py
@@ -2,13 +2,13 @@
 from ..tools import rich_message_consctructor
 
 
-RICH_MEDIA_SETS = rich_message_consctructor('combo')
-RICH_MEDIA_ROLLS = rich_message_consctructor('combo')
-RICH_MEDIA_PIZZA = rich_message_consctructor('combo')
-RICH_MEDIA_GUNCANS = rich_message_consctructor('combo')
-RICH_MEDIA_SUSHI = rich_message_consctructor('combo')
+RICH_MEDIA_SETS = rich_message_consctructor('sets')
+RICH_MEDIA_ROLLS = rich_message_consctructor('rolls')
+RICH_MEDIA_PIZZA = rich_message_consctructor('pizza')
+RICH_MEDIA_GUNCANS = rich_message_consctructor('guncans')
+RICH_MEDIA_SUSHI = rich_message_consctructor('sushi')
 RICH_MEDIA_COMBO = rich_message_consctructor('combo')
-RICH_MEDIA_NUGGETS_WINGS = rich_message_consctructor('combo')
-RICH_MEDIA_MUSSILS = rich_message_consctructor('combo')
-RICH_MEDIA_SAUCES = rich_message_consctructor('combo')
-RICH_MEDIA_DRINKS = rich_message_consctructor('combo')
+RICH_MEDIA_NUGGETS_WINGS = rich_message_consctructor('nuggets')
+RICH_MEDIA_MUSSILS = rich_message_consctructor('mussils')
+RICH_MEDIA_SAUCES = rich_message_consctructor('sauces')
+RICH_MEDIA_DRINKS = rich_message_consctructor('drinks')
